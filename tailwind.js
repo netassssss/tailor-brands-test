e7ab63526e96cd7e4a0745bb8937137df5204557
Neta <neta.seesam@namogoo.com>
2020-08-22 16:00:04 +0300

    fix for build

--- tailwind.js
+++ tailwind.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   theme: {
     extend: {
       colors: {
@@ -16,36 +20,19 @@ module.exports = {
       minHeight: {
         30: '30px',
       },
-      height: (theme) => ({
-        auto: 'auto',
-        ...theme('spacing'),
+      height: {
         200: '200px',
         20: '20px',
         60: '60px',
         '30Of': 'calc(100% - 30px)',
-        full: '100%',
-        screen: '100vh',
-      }),
+      },
       width: {
         20: '20px',
         'special10': 'calc(100% - 20px)',
       },
-      maxWidth: (theme, { breakpoints }) => ({
-        none: 'none',
-        xs: '20rem',
-        sm: '24rem',
-        md: '28rem',
-        lg: '32rem',
-        xl: '36rem',
-        '2xl': '42rem',
-        '3xl': '48rem',
-        '4xl': '56rem',
-        '5xl': '64rem',
-        '6xl': '72rem',
+      maxWidth: {
         360: '360px',
-        full: '100%',
-        ...breakpoints(theme('screens')),
-      }),
+      },
       boxShadow: {
         default: '0px 0px 2px rgba(0, 0, 0, 0.14), 0px 2px 2px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2)',
       },
